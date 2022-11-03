cb6ecbe09d469c13455f0c5ce9d02c34a606ffbc
ElemaGp <elemagodspower@gmail.com>
2022-11-03 15:39:36 -0700

    feat: react-router and Contact component created

--- src/components/items/Items.jsx
+++ src/components/items/Items.jsx
@@ -13,6 +13,7 @@ const Items = () => {
       <a href="https://books.zuri.team/python-for-beginners?ref_id=elemaIK" title="The HNG Design Rules Book gives you the tips & guidelines you need to create truly professional designs. Get yours here now" id="book__python" target="_blank" className="item">Python Books</a>
       <a href="https://background.zuri.team" title="Get top-notch background checks on coders" id="pitch" target="_blank" className="item">Background Checks for Coders</a>
       <a href="https://books.zuri.team/design-rules" title="Get this awesome, free design book by Zuri" id="book__design" target="_blank" className="item">Design Books</a>
+      <a href="/contact" id="contact" className="item">Contact Me</a>
 
       <div className="icons">
         <a href= "https://hng9.slack.com/team/U048AUGBW4T" target="_blank"> <img src={slack} alt="slack icon" /> </a>
