b921adcd360471220ae7c05c2736fd864c726942
ElemaGp <elemagodspower@gmail.com>
2022-11-03 15:00:53 -0700

    feat: Title property for each a tag in Links section

--- src/components/items/Items.jsx
+++ src/components/items/Items.jsx
@@ -9,18 +9,10 @@ const Items = () => {
     <div className="items">
       <a href="https://www.twitter.com/ikechhi" target="_blank" className="item">Twitter Link</a>
       <a href="https://training.zuri.team/" target="_blank" id="btn__zuri" className="item">Zuri Team</a>
-      <a href="https://books.zuri.team" id="books" target="_blank" className="item">Zuri Books
-        <p>find amazing books about design and coding</p>
-      </a>
-      <a href="https://books.zuri.team/python-for-beginners?ref_id=elemaIK" id="book__python" target="_blank" className="item">Python Books
-        <p>The HNG Design Rules Book gives you the tips & guidelines you need to create truly professional designs. Get yours here now!</p>
-      </a>
-      <a href="https://background.zuri.team" id="pitch" target="_blank" className="item">Background Checks for Coders
-        <p>Get top-notch background checks on coders</p>
-      </a>
-      <a href="https://books.zuri.team/design-rules" id="book__design" target="_blank" className="item">Design Books
-        <p>Get this awesome, free design book by Zuri</p>
-      </a>
+      <a href="https://books.zuri.team" title="find amazing books about design and coding" id="books" target="_blank" className="item">Zuri Books</a>
+      <a href="https://books.zuri.team/python-for-beginners?ref_id=elemaIK" title="The HNG Design Rules Book gives you the tips & guidelines you need to create truly professional designs. Get yours here now" id="book__python" target="_blank" className="item">Python Books</a>
+      <a href="https://background.zuri.team" title="Get top-notch background checks on coders" id="pitch" target="_blank" className="item">Background Checks for Coders</a>
+      <a href="https://books.zuri.team/design-rules" title="Get this awesome, free design book by Zuri" id="book__design" target="_blank" className="item">Design Books</a>
 
       <div className="icons">
         <a href= "https://hng9.slack.com/team/U048AUGBW4T" target="_blank"> <img src={slack} alt="slack icon" /> </a>
